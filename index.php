4ee01ee03878c19191d26f136a4e4430b93bf50e
rain1 <ascripting@hotmail.com>
2014-03-02 15:05:28 +0200

    no message

--- index.php
+++ index.php
@@ -3,7 +3,8 @@ include 'funcs.php';
 if (strlen($_GET['a']) == 0){
 	$_GET['a'] = 'new';
 }
-
+$test="abc";
 $file = ReplaceFile(str_replace("{FILE}","po_".$_GET['a'].".html",file_get_contents("main.html")));
+$file = TemplateReplace($file,array());
 print($file)
 ?>
