bdd59b3bb8e361c74a7b34f18c6d0922a70d7ec2
Taavi Gilden <taavigilden@gmail.com>
2014-03-16 16:32:38 +0200

    no message

---
+++ Settings.php
@@ -0,0 +1,7 @@
+<?php
+$DATABASE_SERVER = "localhost";
+$DATABASE_PORT = "";
+$DATABASE_USER = "root";
+$DATABASE_NAME = "test_database";
+$DATABASE_PASSWORD = "";
+?>
--- includes/ucp/ucp.php
+++ includes/ucp/ucp.php
@@ -1,50 +1,60 @@
 <?php
 
 function ProccessRegistration(){
+    $_GET['a']='notification';
     global $TabsDataJS,$FILE_PATH,$CURRENT_MODULE,$Notification,$Loginform;
-    $_POST['UserPassword'] = md5($_POST['UserPasswordConfirm']);
-    $_POST['UserPasswordConfirm'] = md5($_POST['UserPasswordConfirm']);
+
+    $title = "none";
     $TabsDataJS = "var TabsData = [];TabsData[\"0\"] = [];TabsData[\"0\"][\"link\"] = \"15\";TabsData[\"0\"][\"text\"] = \"REGISTER\";";
     $FILE_PATH[$CURRENT_MODULE] = '../theme/default/UCP/register.html';
+    print "s2";
     if($_POST['UserPassword'] != $_POST['UserPasswordConfirm']){
         $FILE_PATH[$CURRENT_MODULE] = '../theme/default/UCP/failure.html';
+        $title = "Registration error!";
         $Notification = "Passwords mismatch <br> <a href=\"#\" onclick=\"history.go(-1)\">Retry</a> or <a href=\"../\">Go to board index</a>";
         return;
     }
     if($_POST['UserPassword'] == "" || $_POST['UserName'] == ""|| $_POST['UserEmail'] == ""){
         $FILE_PATH[$CURRENT_MODULE] = '../theme/default/UCP/failure.html';
+        $title = "Registration error!";
         $Notification = "You did not fill all the required fields <br> <a href=\"#\" onclick=\"history.go(-1)\">Retry</a> or <a href=\"../\">Go to board index</a>";
         return;
     }
     if(UserGetIDByName($_POST['UserName'])!=""){
         $FILE_PATH[$CURRENT_MODULE] = '../theme/default/UCP/failure.html';
+        $title = "Registration error!";
         $Notification = "User already exists <br> <a href=\"#\" onclick=\"history.go(-1)\">Retry</a> or <a href=\"../\">Go to board index</a>";
         return;
     }
-    if($_POST['agreement']!="1" || !isset($_POST['agreement'])){
+    /*if($_POST['agreement']!="1" || !isset($_POST['agreement'])){
         $FILE_PATH[$CURRENT_MODULE] = '../theme/default/UCP/failure.html';
         $Notification = "You did not accept terms of use <br> <a href=\"#\" onclick=\"history.go(-1)\">Retry</a> or <a href=\"../\">Go to board index</a>";
         return;
-    }
-    if($_POST['confirmation']!="Precarious"){
+    }*/
+    /*if($_POST['confirmation']!="Precarious"){
         $FILE_PATH[$CURRENT_MODULE] = '../theme/default/UCP/failure.html';
         $Notification = "You did not answer confirmation code right <br> <a href=\"#\" onclick=\"history.go(-1)\">Retry</a> or <a href=\"../\">Go to board index</a>";
         return;
-    }
-    if(!mkuser()){
+    }*/
+    if(!UserAdd($_POST['UserName'], $_POST['UserPassword'], $_POST['UserEmail'], "" , time(), 0, 0, 0 )){
         $FILE_PATH[$CURRENT_MODULE] = '../theme/default/UCP/failure.html';
+        $title = "Registration error!";
         $Notification = "Account creation failed for unknown reason. <br> <a href=\"#\" onclick=\"history.go(-1)\">Retry</a> or <a href=\"../\">Go to board index</a>";
     }else{
         $FILE_PATH[$CURRENT_MODULE] = '../theme/default/UCP/success.html';
+        $title = "Registration success!";
         $Notification = "Your account have been successfully created <br> <a href=\"#\" onclick=\"history.go(-1)\">Retry</a> or <a href=\"../\">Go to board index</a>";
         Login($_POST['UserName'],$_POST['UserPassword'],true, 0);
         $Loginform = GetLoginForm();
-        GroupAddMember(3, $_POST['UserName'], 1, true);//add user to registered users
+        //GroupAddMember(3, $_POST['UserName'], 1, true);//add user to registered users
         header('Refresh: 5; URL=../');
     }
     return;
 }
     if($_GET['a'] == 'signin'){
+         $_GET['a'] = 'notification';
+        $title = "Login";
+        $Notification = "Login successful!";
         $uid = Login($_POST['UserName'],$_POST['UserPassword'],CheckboxToBool($_POST['remember']));
         if($uid > 1){
             header('Refresh: 5; URL=./');
@@ -72,6 +82,9 @@ function ProccessRegistration(){
 
 
     if($_GET['a'] == 'logout'){
+        $_GET['a'] = 'notification';
+        $title = "Logout";
+        $Notification = "Logout successful!";
         SessionDelete($_COOKIE['Session']);
         header('Refresh: 5; URL=./');
     }
